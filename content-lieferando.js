c90dacd2df0ad6e741729fb356b0c51496462735
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-07-13 15:47:00 +0200

    Add simple button to restaurant-header and show dialog on click

--- content-lieferando.js
+++ content-lieferando.js
@@ -1,10 +1,20 @@
 // TODO: Support different languages and URL structures
 
+const SELECTOR_DISHES = "[data-qa=item]";
+const SELECTOR_RESTAURANT_HEADER = "[data-qa=restaurant-header]";
+const EXTRACT_IDENTIFIER_REGEX = /\/speisekarte\/([\w-]*)/;
+const RESTAURANT_NOTES_BUTTON = `
+<div style="display: inline-flex">
+    <button class="dn-restaurant-notes" popovertarget="dn-restaurant-notes-popover">x</button>
+    <dialog id="dn-restaurant-notes-popover" popover>Restaurant Notes Popover</dialog>
+</div>
+`;
+
 (async () => {
     async function waitForPageLoaded() {
         for (let i = 0; i < 5; ++i) {
             console.log("Check for loaded page");
-            if (document.querySelectorAll("[data-qa=item]").length > 0) {
+            if (document.querySelectorAll(SELECTOR_DISHES).length > 0) {
                 return;
             }
 
@@ -20,7 +30,7 @@
         }
 
         const { pathname } = document.location;
-        const result = pathname.match(/\/speisekarte\/([\w-]*)/);
+        const result = pathname.match(EXTRACT_IDENTIFIER_REGEX);
         if (!result) {
             return undefined;
         }
@@ -33,47 +43,57 @@
     }
 
     function attachUiElements() {
-        const dishesEl = document.querySelectorAll("[data-qa=item]");
-        console.log(dishesEl);
-        if (!dishesEl) {
+        const headerEl = document.querySelector(SELECTOR_RESTAURANT_HEADER);
+        if (!headerEl) {
             return;
         }
 
-        for (let i = 0; i < dishesEl.length; ++i) {
-            attachUiElementsForDish(dishesEl[i]);
+        const insertAtEl = headerEl.querySelector("[data-qa=heading]");
+        if (!insertAtEl) {
+            return;
         }
+
+        const domParser = new DOMParser();
+        const elem = domParser.parseFromString(
+            RESTAURANT_NOTES_BUTTON,
+            "text/html"
+        ).body.firstElementChild;
+        console.log(elem);
+
+        insertAtEl.appendChild(elem);
 
         return;
     }
 
-    function attachUiElementsForDish(dishEl) {
-        if (!dishEl) {
-            return;
-        }
-
-        const headingEl = dishEl.querySelector("[data-qa=heading]");
-        if (!headingEl) {
-            return;
-        }
-
-        const dishname = headingEl.innerText;
-        console.log(dishname);
+    function onClickRestaurantNotes(evt) {
+        console.log("click restaurant notes");
     }
 
     function attachEventListeners() {
+        document.addEventListener("click", (evt) => {
+            const t = evt.target;
+            if (!t) {
+                return;
+            }
+
+            if (t.classList.contains("dn-restaurant-notes")) {
+                onClickRestaurantNotes(evt);
+                return;
+            }
+
+            // TODO
+        });
+
         return;
     }
 
     function initialize() {
-        console.log("Delivery Notes");
         const identifier = extractIdentifierFromUrl();
-        console.log(identifier);
         if (!identifier) {
             return;
         }
         console.log(`Found identifier: ${identifier}`);
         const notes = loadNotesByIdentifier(identifier);
-        console.log(notes);
 
         attachUiElements();
         attachEventListeners();
