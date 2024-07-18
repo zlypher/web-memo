98a472344d0ef45d9235a215807530305e440364
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-07-18 22:57:19 +0200

    First try to integrate bundling via rollup into the process

--- src/content-lieferando.js
+++ src/content-lieferando.js
@@ -1,4 +1,5 @@
 // TODO: Support different languages and URL structures
+import { loadNotesByIdentifier, saveNotes } from "./note-storage";
 
 const SELECTOR_DISHES = "[data-qa=item]";
 const SELECTOR_RESTAURANT_HEADER = "[data-qa=restaurant-header]";
@@ -50,145 +51,141 @@ const getRestaurantNotesTemplate = (notes) => `
 </div>
 `;
 
+let identifier;
+
+/**
+ * Lieferando loads the dishes async. This methods tries to find the
+ * dishes in the dom. If it can't find it, it waits a second and retries.
+ * After 5 tries, it fails and we can't proceed.
+ */
+async function waitForPageLoaded() {
+    for (let i = 0; i < 5; ++i) {
+        if (document.querySelectorAll(SELECTOR_DISHES).length > 0) {
+            return;
+        }
+
+        await new Promise((res) => setTimeout(res, 1000));
+    }
+
+    throw new Error("Didn't detect page loaded in time");
+}
+
+/**
+ * Try to extract and return an unique identifier for the restaurant
+ * from the pathname of the URL. If the extraction fails for any
+ * reason, return undefined.
+ */
+function extractIdentifierFromUrl(pathname) {
+    if (!pathname) {
+        return undefined;
+    }
+
+    const result = pathname.match(EXTRACT_IDENTIFIER_REGEX);
+    if (!result) {
+        return undefined;
+    }
+
+    return result[1];
+}
+
+// /**
+//  * Load the notes for the current restaurant identified via the identifier
+//  * from the sync storage. If there are no notes yet, or there is an error,
+//  * return undefined.
+//  */
+// async function loadNotesByIdentifier(identifier) {
+//     const key = `NOTES_${identifier}`;
+
+//     try {
+//         let results = await browser.storage.sync.get(key);
+//         if (results?.hasOwnProperty(key)) {
+//             return results[key];
+//         }
+
+//         return undefined;
+//     } catch (e) {
+//         console.error(e);
+//         return undefined;
+//     }
+// }
+
+// /**
+//  * Saves the notes for the current restaurant identified by the
+//  * identifier.
+//  */
+// async function saveNotes(identifier, notes) {
+//     const key = `NOTES_${identifier}`;
+
+//     try {
+//         await browser.storage.sync.set({ [key]: notes });
+//     } catch (e) {
+//         console.error(e);
+//     }
+
+//     closePopover();
+// }
+
+function closePopover() {
+    document.querySelector("#dn-restaurant-notes-popover").hidePopover();
+}
+
+/**
+ * Attach all necessary UI Elements, for this extension to work, to the DOM.
+ * If we can't find the correct place to insert into the DOM, throw an error.
+ */
+function attachUiElements(notes) {
+    const headerEl = document.querySelector(SELECTOR_RESTAURANT_HEADER);
+    if (!headerEl) {
+        throw new Error("Couldn't find Header element to attach elements to");
+    }
+
+    const insertAtEl = headerEl.querySelector("[data-qa=heading]");
+    if (!insertAtEl) {
+        throw new Error("Couldn't find Heading element to attach elements to");
+    }
+
+    const domParser = new DOMParser();
+    const elem = domParser.parseFromString(
+        getRestaurantNotesTemplate(notes),
+        "text/html"
+    ).body.firstElementChild;
+
+    insertAtEl.appendChild(elem);
+
+    return;
+}
+
+/**
+ * OnClick-Handler of save button of the popup. Get's the notes from the
+ * text element and tries to save it.
+ */
+function onClickRestaurantNotesSave() {
+    const notes = document.querySelector(SELECTOR_NOTES_TEXT).value;
+    saveNotes(identifier, notes);
+}
+
+/**
+ * Attaches all necessary event listeners, for this extension to work.
+ */
+function attachEventListeners() {
+    document.addEventListener("click", (evt) => {
+        const t = evt.target;
+        if (!t) {
+            return;
+        }
+
+        if (t.classList.contains("dn-restaurant-notes__save")) {
+            onClickRestaurantNotesSave();
+            return;
+        }
+
+        // TODO
+    });
+
+    return;
+}
+
 (async () => {
-    let identifier;
-
-    /**
-     * Lieferando loads the dishes async. This methods tries to find the
-     * dishes in the dom. If it can't find it, it waits a second and retries.
-     * After 5 tries, it fails and we can't proceed.
-     */
-    async function waitForPageLoaded() {
-        for (let i = 0; i < 5; ++i) {
-            if (document.querySelectorAll(SELECTOR_DISHES).length > 0) {
-                return;
-            }
-
-            await new Promise((res) => setTimeout(res, 1000));
-        }
-
-        throw new Error("Didn't detect page loaded in time");
-    }
-
-    /**
-     * Try to extract and return an unique identifier for the restaurant
-     * from the pathname of the URL. If the extraction fails for any
-     * reason, return undefined.
-     */
-    function extractIdentifierFromUrl(pathname) {
-        if (!pathname) {
-            return undefined;
-        }
-
-        const result = pathname.match(EXTRACT_IDENTIFIER_REGEX);
-        if (!result) {
-            return undefined;
-        }
-
-        return result[1];
-    }
-
-    /**
-     * Load the notes for the current restaurant identified via the identifier
-     * from the sync storage. If there are no notes yet, or there is an error,
-     * return undefined.
-     */
-    async function loadNotesByIdentifier(identifier) {
-        const key = `NOTES_${identifier}`;
-
-        try {
-            let results = await browser.storage.sync.get(key);
-            if (results?.hasOwnProperty(key)) {
-                return results[key];
-            }
-
-            return undefined;
-        } catch (e) {
-            console.error(e);
-            return undefined;
-        }
-    }
-
-    /**
-     * Saves the notes for the current restaurant identified by the
-     * identifier.
-     */
-    async function saveNotes(identifier, notes) {
-        const key = `NOTES_${identifier}`;
-
-        try {
-            await browser.storage.sync.set({ [key]: notes });
-        } catch (e) {
-            console.error(e);
-        }
-
-        closePopover();
-    }
-
-    function closePopover() {
-        document.querySelector("#dn-restaurant-notes-popover").hidePopover();
-    }
-
-    /**
-     * Attach all necessary UI Elements, for this extension to work, to the DOM.
-     * If we can't find the correct place to insert into the DOM, throw an error.
-     */
-    function attachUiElements(notes) {
-        const headerEl = document.querySelector(SELECTOR_RESTAURANT_HEADER);
-        if (!headerEl) {
-            throw new Error(
-                "Couldn't find Header element to attach elements to"
-            );
-        }
-
-        const insertAtEl = headerEl.querySelector("[data-qa=heading]");
-        if (!insertAtEl) {
-            throw new Error(
-                "Couldn't find Heading element to attach elements to"
-            );
-        }
-
-        const domParser = new DOMParser();
-        const elem = domParser.parseFromString(
-            getRestaurantNotesTemplate(notes),
-            "text/html"
-        ).body.firstElementChild;
-
-        insertAtEl.appendChild(elem);
-
-        return;
-    }
-
-    /**
-     * OnClick-Handler of save button of the popup. Get's the notes from the
-     * text element and tries to save it.
-     */
-    function onClickRestaurantNotesSave() {
-        const notes = document.querySelector(SELECTOR_NOTES_TEXT).value;
-        saveNotes(identifier, notes);
-    }
-
-    /**
-     * Attaches all necessary event listeners, for this extension to work.
-     */
-    function attachEventListeners() {
-        document.addEventListener("click", (evt) => {
-            const t = evt.target;
-            if (!t) {
-                return;
-            }
-
-            if (t.classList.contains("dn-restaurant-notes__save")) {
-                onClickRestaurantNotesSave();
-                return;
-            }
-
-            // TODO
-        });
-
-        return;
-    }
-
     try {
         // Step 1: Wait for Page Load (since it is async)
         await waitForPageLoaded();
