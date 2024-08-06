d2b8d667dd9a58f9325e87c150f61e0dbaf2637c
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-08-06 12:53:08 +0200

    Add analytics with mixpanel to implement #9 and disable all identifiable information. Provide a simple way to opt-out of anonym. tracking.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,22 @@
 import browser from "webextension-polyfill";
+import mixpanel from "mixpanel-browser";
 import { loadNotesByIdentifier, saveNotes } from "../note-storage";
 import { extractIdentifierFromUrl, isRelevantUrl } from "../url-identifier";
 import { localizeHtmlPage } from "../localize-html";
+
+mixpanel.init("b6a9bb270b20eadfaf653f4bb86b9412", {
+    track_pageview: true,
+    api_host: "https://api-eu.mixpanel.com",
+    ip: false,
+    persistence: "localStorage",
+    property_blacklist: [
+        "$os",
+        "$screen_height",
+        "$screen_width",
+        "$device_id",
+    ],
+});
+mixpanel.identify("web-ext");
 
 let identifier;
 
@@ -16,6 +31,12 @@ async function getCurrentActiveTabUrl() {
 
 async function initialize() {
     localizeHtmlPage();
+
+    if (mixpanel.has_opted_out_tracking()) {
+        document
+            .querySelector(".dn-restaurant-notes__dnt")
+            .classList.add("hidden");
+    }
 
     const url = await getCurrentActiveTabUrl();
     // Step 1: Check URL
@@ -36,8 +57,15 @@ async function initialize() {
     }
 
     document
+        .querySelector(".dn-restaurant-notes__dnt")
+        .addEventListener("click", () => {
+            mixpanel.opt_out_tracking();
+        });
+
+    document
         .querySelector(".dn-restaurant-notes__save")
         .addEventListener("click", () => {
+            mixpanel.track("Save Notes");
             const notes = document.querySelector(
                 ".dn-restaurant-notes__text"
             ).value;
