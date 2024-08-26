7a746f59033e0df690b8653195bb42a456616ca5
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-08-26 21:04:36 +0200

    Remove tracking from installed page

--- src/views/installed/installed.js
+++ src/views/installed/installed.js
@@ -2,10 +2,6 @@ import { localizeHtmlPage } from "../../utils/localize-html";
 
 async function initialize() {
     localizeHtmlPage();
-
-    document.querySelector(".dn-restaurant-notes__toggle-tracking").addEventListener("click", () => {
-        // TODO
-    })
 }
 
 document.addEventListener("DOMContentLoaded", initialize);
